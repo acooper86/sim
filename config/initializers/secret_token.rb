ba2d377cc050b5d112b2d12431d9eeedae1a556e
Adam Cooper <fluke.a@gmail.com>
2012-03-27 20:24:31 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sim::Application.config.secret_token = '5dfff7bfbfbc5bc118c772ecd4bb75e04372134a68484ebdc8f063f969fb50f737c416c680481ab685ca07edbb3af6cac35a6ab3ca841ffadfad195258469dc7'
