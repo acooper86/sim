e8eea7d47793ee720a6c0c1d80c113ce6043ecb9
Adam Cooper <fluke.a@gmail.com>
2012-07-28 10:45:25 -0500

    fixing migration for pages order

--- db/migrate/20120629161028_remove_order_from_pages.rb
+++ db/migrate/20120629161028_remove_order_from_pages.rb
@@ -1,11 +1,9 @@
 class RemoveOrderFromPages < ActiveRecord::Migration
   def up
-    remove_column :pages, :order
   	change_column :pages, :child, :boolean
   end
 
   def down
-    add_column :pages, :order, :integer
     change_column :pages, :child, :integer
   end
 end
